56aef3dfbbbb29204e8ff28399d5bd8b5a68bb1b
Kac-B <kacbier@wp.pl>
2021-12-06 23:18:02 +0100

    update controller

---
+++ src/Controller/KursNbpController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class KursNbpController extends AbstractController
+{
+    /*#[Route('/kurs/nbp', name: 'kurs_nbp')]*/
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/KursNbpController.php',
+        ]);
+    }
+}
